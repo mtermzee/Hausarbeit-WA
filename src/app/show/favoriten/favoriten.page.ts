6d3df1b5e48214555a631038a1ac4a973511f7c7
mtermzee <mohammad.termzee@hs-osnabrueck.de>
2022-02-02 10:53:37 +0100

    .

--- src/app/show/favoriten/favoriten.page.ts
+++ src/app/show/favoriten/favoriten.page.ts
@@ -63,7 +63,7 @@ export class FavoritenPage implements OnInit {
     window.open(homepage, '_blank');
    }
   
-  accountLink(item) {
+   accountLink(item) {
      // https://www.reddit.com/r/angular/comments/9n0uby/how_can_i_use_an_ngfor_and_an_ngif_so_that_the/
      if (item.type === 'movie') {
        console.log("Movie");
